b85e0b99dd129d2dfd5e7966d920e3db9e4d2938
masatora <atarashi.masatora@gmail.com>
2022-01-16 03:56:25 +0900

    test s3 upload

---
+++ go.mod
@@ -0,0 +1,7 @@
+module go-image-processing-test
+
+go 1.17
+
+require github.com/aws/aws-sdk-go v1.42.35
+
+require github.com/jmespath/go-jmespath v0.4.0 // indirect
--- main.go
+++ main.go
@@ -4,26 +4,81 @@ import (
 	"image"
 	"image/color"
 	"image/png"
+	"log"
 	"math"
 	"os"
+
+	"github.com/aws/aws-sdk-go/service/s3"
+
+	"github.com/aws/aws-sdk-go/aws"
+
+	"github.com/aws/aws-sdk-go/service/s3/s3manager"
+
+	"github.com/aws/aws-sdk-go/aws/session"
 )
 
 func main() {
-	f, err := os.Open("assets/screenshot.png")
-	if err != nil {
-		panic(err)
-	}
-	defer f.Close()
+	//f, err := os.Open("assets/screenshot.png")
+	//if err != nil {
+	//	panic(err)
+	//}
+	//defer f.Close()
+	//
+	//img, err := png.Decode(f)
+	//if err != nil {
+	//	panic(err)
+	//}
+	//
+	//err = invertImgColor(img)
+	//if err != nil {
+	//	panic(err)
+	//}
+	invertAndUploadTest()
+}
 
-	img, err := png.Decode(f)
-	if err != nil {
-		panic(err)
+func invertAndUploadTest() {
+	sess := session.Must(session.NewSessionWithOptions(session.Options{
+		SharedConfigState: session.SharedConfigEnable,
+	}))
+	downloader := s3manager.NewDownloader(sess)
+	f, _ := os.Create("assets/s3test.png")
+	downloader.Download(f, &s3.GetObjectInput{
+		Bucket: aws.String("web-snapshot-s3-us-east-1"),
+		Key:    aws.String("pdf/6300/screenshot.png"),
+	})
+	img, _ := png.Decode(f)
+	bounds := img.Bounds()
+	w, h := bounds.Max.X, bounds.Max.Y
+	rgbaScale := image.NewRGBA(image.Rectangle{
+		Min: image.Point{0, 0},
+		Max: image.Point{w, h},
+	})
+	for x := 0; x < w; x++ {
+		for y := 0; y < h; y++ {
+			imgColor := img.At(x, y)
+			rr, gg, bb, aa := imgColor.RGBA()
+			var max = int64(255)
+			nr := max - int64(int8(rr))
+			ng := max - int64(int8(gg))
+			nb := max - int64(int8(bb))
+			invertColor := color.RGBA{
+				R: uint8(nr),
+				G: uint8(ng),
+				B: uint8(nb),
+				A: uint8(aa),
+			}
+			rgbaScale.Set(x, y, invertColor)
+		}
 	}
-
-	err = invertImgColor(img)
-	if err != nil {
-		panic(err)
-	}
+	newF, _ := os.Create("assets/s3test-after.png")
+	png.Encode(newF, rgbaScale)
+	a, _ := os.Open("assets/s3test-after.png")
+	uploader := s3manager.NewUploader(sess)
+	uploader.Upload(&s3manager.UploadInput{
+		Bucket: aws.String("web-snapshot-s3-us-east-1"),
+		Key:    aws.String("tmp/test.png"),
+		Body:   a,
+	})
 }
 
 func invertImgColor(img image.Image) error {
@@ -57,6 +112,22 @@ func invertImgColor(img image.Image) error {
 	}
 	defer newF.Close()
 	png.Encode(newF, rgbaScale)
+
+	a, _ := os.Open("assets/invertScreenshot.png")
+	// upload to s3
+	sess := session.Must(session.NewSessionWithOptions(session.Options{
+		SharedConfigState: session.SharedConfigEnable,
+	}))
+	uploader := s3manager.NewUploader(sess)
+	_, err = uploader.Upload(&s3manager.UploadInput{
+		Bucket: aws.String("web-snapshot-s3-us-east-1"),
+		Key:    aws.String("tmp/test.png"),
+		Body:   a,
+	})
+	if err != nil {
+		log.Fatal(err)
+	}
+
 	return nil
 }
 
